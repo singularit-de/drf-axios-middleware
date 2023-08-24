e6f63f18ff6473beb659629cc2fb2051b8a69b93
robin.kehl <robin.kehl@singular-it.de>
2023-08-24 15:08:26 +0200

    refactor: prepare release

--- release.config.cjs
+++ release.config.cjs
@@ -3,20 +3,5 @@ module.exports = {
     'master', // deprecated
     {name: 'beta', prerelease: true},
   ],
-  // plugins: [
-  //   '@semantic-release/commit-analyzer',
-  //   '@semantic-release/release-notes-generator',
-  //   // [
-  //   //   '@semantic-release/exec',
-  //   //   {
-  //   //     // eslint-disable-next-line no-template-curly-in-string
-  //   //     verifyReleaseCmd: 'echo "export default \'${nextRelease.version}\'" > "src/version.ts"',
-  //   //   },
-  //   // ],
-  //   ['@semantic-release/npm', {
-  //     npmPublish: false,
-  //     tarballDir: 'dist',
-  //   }],
-  // ],
 }
 
