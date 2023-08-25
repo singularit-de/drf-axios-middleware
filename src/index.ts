14da39125a41d9a40683a74eeb73ce7bff503fe4
robin.kehl <robin.kehl@singular-it.de>
2023-08-25 12:53:30 +0200

    feat: export `convertFilterSetConfig`

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-export {applyDRFInterceptor as default} from './middleware'
+export {applyDRFInterceptor as default, convertFilterSetConfig} from './middleware'
 export type {DRFFilters as DefaultDRFFilters, FilterHandler, FilterSetConfig, DRFAxiosConfig} from './types'
 // eslint-disable-next-line import/export
 export * from './axios'
