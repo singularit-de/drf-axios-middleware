3df878466a3e02b1d8382ed8f224f2d06ee9906e
robin.kehl <robin.kehl@singular-it.de>
2023-08-28 10:48:19 +0200

    docs: add search

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -14,11 +14,6 @@ export default defineConfig({
       {text: 'Imprint', link: 'https://www.singular-it.de/impressum'},
     ],
 
-    docFooter: {
-      prev: 'Vorherige Seite',
-      next: 'Nächste Seite',
-    },
-
     sidebar: [
       {
         text: 'Guide',
@@ -45,7 +40,59 @@ export default defineConfig({
     socialLinks: [
       {icon: 'github', link: 'https://github.com/singularit-de/drf-axios-middleware'},
     ],
+    search: {
+      provider: 'algolia',
+      options: {
+        appId: 'ON2RIH2H4Q',
+        apiKey: '4db353c83a9c76fb04aea88371f6d1b0',
+        indexName: 'drf-axios-middleware',
+        locales: {
+          de: {
+            placeholder: 'Suchen',
+            translations: {
+              button: {
+                buttonText: 'Suchen',
+                buttonAriaLabel: 'Suchen',
+              },
+              modal: {
+                searchBox: {
+                  resetButtonTitle: 'Zurücksetzen',
+                  resetButtonAriaLabel: 'Zurücksetzen',
+                  cancelButtonText: 'Abbrechen',
+                  cancelButtonAriaLabel: 'Abbrechen',
+                },
+                startScreen: {
+                  recentSearchesTitle: 'Zuletzt gesucht',
+                  noRecentSearchesText: 'Keine zuletzt gesuchten Begriffe',
+                  saveRecentSearchButtonTitle: 'Zu den zuletzt gesuchten Begriffen hinzufügen',
+                  removeRecentSearchButtonTitle: 'Zuletzt gesuchten Begriffe entfernen',
+                  favoriteSearchesTitle: 'Favoriten',
+                  removeFavoriteSearchButtonTitle: 'Favoriten entfernen',
+                },
+                errorScreen: {
+                  titleText: 'Es ist ein Fehler aufgetreten',
+                  helpText: 'Bitte versuche es später noch einmal',
+                },
+                footer: {
+                  selectText: 'Auswählen',
+                  navigateText: 'Navigieren',
+                  closeText: 'Schließen',
+                  searchByText: 'Suche von',
+                },
+                noResultsScreen: {
+                  noResultsText: 'Keine Ergebnisse gefunden für',
+                  suggestedQueryText: 'Meintest du',
+                  reportMissingResultsText: 'Fehlende Ergebnisse melden',
+                  reportMissingResultsLinkText: 'Melden',
+                },
+              },
+            },
+          },
+        },
+      },
+    },
   },
+
   locales: {
     root: {
       label: 'English',
@@ -61,6 +108,13 @@ export default defineConfig({
           {text: 'Changelogs', link: 'https://github.com/singularit-de/drf-axios-middleware/releases'},
           {text: 'Impressum', link: 'https://www.singular-it.de/impressum'},
         ],
+        docFooter: {
+          prev: 'Vorherige Seite',
+          next: 'Nächste Seite',
+        },
+        outline: {
+          label: 'Auf dieser Seite',
+        },
 
         sidebar: [
           {
