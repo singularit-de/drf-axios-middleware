77d8a1041612643fc7ba34dcf6f4b49aa0c0b51a
Tim Streicher <tim.streicher@singular-it.de>
2023-11-30 13:04:16 +0100

    feat: export guards

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 export {applyDRFInterceptor as default, convertFilterSetConfig} from './middleware'
+export * from './guards'
 export type {DRFFilters as DefaultDRFFilters, FilterHandler, FilterSetConfig, DRFAxiosConfig} from './types'
 // eslint-disable-next-line import/export
 export * from './axios'
